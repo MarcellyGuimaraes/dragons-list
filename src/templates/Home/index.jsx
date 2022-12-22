c1d156307d695cab2a23c26bbb8c3e174103c9f0
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-22 19:01:16 -0300

    retirando div desnecessária

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -19,27 +19,25 @@ const Home = () => {
 
   return (
     <div className="flex flex-col justify-center m-2">
-      <h1 className="text-center m-4">Tabela de Dragões</h1>
-      <div className="w-full overflow-x-auto mb-6 relative shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <HeaderTable />
-          <tbody>
-            {dragons.map((dragon) => {
-              return (
-                <tr
-                  key={dragon.id}
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-                >
-                  <th className="py-4 px-6">{dragon.name}</th>
-                  <td className="py-4 px-6">{dragon.type}</td>
-                  <td className="py-4 px-6">{dragon?.createdAt}</td>
-                  <ButtonTable idDragon={dragon.id} />
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      <h1 className="text-center m-4">Gerador aleatório de dragões</h1>
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <HeaderTable />
+        <tbody>
+          {dragons.map((dragon) => {
+            return (
+              <tr
+                key={dragon.id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <th className="py-4 px-6">{dragon.name}</th>
+                <td className="py-4 px-6">{dragon.type}</td>
+                <td className="py-4 px-6">{dragon?.createdAt}</td>
+                <ButtonTable idDragon={dragon.id} />
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
