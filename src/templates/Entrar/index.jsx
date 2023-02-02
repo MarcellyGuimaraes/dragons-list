0e268d52936342f762e6f0e9ba91af32b4469b1a
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-02-02 18:17:10 -0300

    feat: adicionando meu nome no login

--- src/templates/Entrar/index.jsx
+++ src/templates/Entrar/index.jsx
@@ -23,7 +23,7 @@ const Entrar = () => {
   return (
     <div className="flex flex-col bg-slate-400 items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <p className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-        WoopSicredi Challenge
+        WoopSicredi Celly
       </p>
       <div className="w-full bg-white bg-opacity-40 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <form
